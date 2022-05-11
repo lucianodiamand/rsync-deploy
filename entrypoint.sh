ddea5f7f969dd068bc8d06607b51224993b0c941
lucianodiamand <85711762+lucianodiamand@users.noreply.github.com>
2022-05-11 10:47:53 -0300

    Add change directory permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,3 +15,5 @@ DSN="$INPUT_REMOTE_USER@$INPUT_REMOTE_HOST"
 
 # Despliegue.
 sh -c "rsync $SWITCHES -e '$RSH' $LOCAL_PATH $DSN:$INPUT_REMOTE_PATH"
+ssh -i /root/.ssh/id_rsa -tt $DSN "chown -R www-data:www-data $INPUT_REMOTE_PATH"
+ssh -i /root/.ssh/id_rsa -tt $DSN "chmod 775 -R $INPUT_REMOTE_PATH"

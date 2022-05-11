f9cf176d6c402cfe89bbb0cb91d9cfdfa917aa67
lucianodiamand <85711762+lucianodiamand@users.noreply.github.com>
2022-05-11 14:55:59 -0300

    Add default permissions variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Inicia el agente SSH y carga la key.
 source agent-start "$GITHUB_ACTION"
@@ -10,11 +10,12 @@ set -eu
 # Variables.
 SWITCHES="$INPUT_SWITCHES"
 OWNER="${INPUT_OWNER:-apache}"
+PERMISSIONS="${INPUT_PERMISSIONS:-755}"
 RSH="ssh -o StrictHostKeyChecking=no -p $INPUT_REMOTE_PORT $INPUT_RSH"
 LOCAL_PATH="$GITHUB_WORKSPACE/$INPUT_PATH"
 DSN="$INPUT_REMOTE_USER@$INPUT_REMOTE_HOST"
 
 # Despliegue.
-sh -c "rsync $SWITCHES -e '$RSH' $LOCAL_PATH $DSN:$INPUT_REMOTE_PATH"
+bash -c "rsync $SWITCHES -e '$RSH' $LOCAL_PATH $DSN:$INPUT_REMOTE_PATH"
 $RSH -tt $DSN "chown -R $OWNER:$OWNER $INPUT_REMOTE_PATH"
-$RSH -tt $DSN "chmod $INPUT_PERMISSIONS -R $INPUT_REMOTE_PATH"
+$RSH -tt $DSN "chmod $PERMISSIONS -R $INPUT_REMOTE_PATH"
